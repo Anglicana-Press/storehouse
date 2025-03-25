f2332d3d0e8d613e77861d7dde0448bc7ed8afda
Adam Womble <adamlwomble@gmail.com>
2025-03-25 15:46:45 -0500

    feat(layout): update component to add rudimentary structure

--- application/src/routes/+layout.svelte
+++ application/src/routes/+layout.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
 	import '../app.css';
+	import Lettermark from '$lib/components/Lettermark.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="shadow-purple-med/50 fixed bottom-1 left-1 top-1 w-7 rounded-lg bg-white shadow">
+	<nav>
+		<ol>
+			<li>
+				<a
+					href="/"
+					class="bg-purple-min flex size-7 items-center justify-center rounded-b-none rounded-t-lg text-white"
+				>
+					<span class="sr-only">Home</span>
+					<Lettermark class="size-4" />
+				</a>
+			</li>
+		</ol>
+	</nav>
+</header>
+<main class="py-3 pl-12 pr-4">
+	{@render children()}
+</main>
